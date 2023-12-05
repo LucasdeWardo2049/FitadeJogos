b4f0f5c78792e2863615d63d2ffbd67a4ce40019
LucasDeWardo2049 <>
2023-12-05 11:09:37 -0400

    Useroutes

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,10 @@ app.use(express.json())
 app.use(cors({ credentials: true, origin: 'https://localhost:3000' }))
 
 // Pasta pública para imagens
-app.use(express.static('public/'))
+app.use(express.static('public'))
 
-// Rotas
+const UserRoutes = require('Routes/UserRoutes')
+
+app.use('/users', UserRoutes)
 
 app.listen(5000) // Inicia o servidor na porta 5000
